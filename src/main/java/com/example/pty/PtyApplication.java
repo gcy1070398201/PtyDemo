8039482e6ecdf4b4394506477af984258a2f1dd1
gcy <gu1070398201@sina.com>
2019-10-23 20:19:48 +0800

    Initial commit

---
+++ src/main/java/com/example/pty/PtyApplication.java
@@ -0,0 +1,13 @@
+package com.example.pty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pty/PtyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
